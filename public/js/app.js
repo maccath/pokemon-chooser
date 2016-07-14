ad120789614cb04c56faba9c578d7e20937a33d3
Katy Ereira <katy.ereira@castletrust.co.uk>
2016-07-14 14:32:50 +0100

    Add list of types to application.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,19 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-        message: 'Gotta Catch \'Em All!'
+        message: 'Gotta Catch \'Em All!',
+        types: []
+    },
+    methods: {
+        getTypes: function () {
+            this.$http.get('http://pokeapi.co/api/v2/type/').then(function (response) {
+                this.types = response.data.results;
+            }, function (response) {
+                // Todo: handle an error
+            });
+        }
     }
 });
+
+// On load
+app.getTypes();
